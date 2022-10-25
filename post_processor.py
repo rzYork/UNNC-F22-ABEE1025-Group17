4a1dbb55e9904d6774a86a1b841b9f2b13bb9eb1
ssysc6 <115373455+ssysc6@users.noreply.github.com>
2022-10-25 17:58:46 +0800

    done the method of post_processor

--- post_processor.py
+++ post_processor.py
@@ -22,4 +22,36 @@ def eplus_to_datetime(date_str):
 #            iii. have plot_title as the plot title
 #            iv. each line in the plot has the key in output_paths as the legend 
 def plot_1D_results(output_paths,plot_column_name,y_axis_title,plot_title):
-    pass
+	
+	fontsize = 20
+	fig, axs = plt.subplots(1, 1, figsize=(20,10))
+	
+	for item in output_paths.keys():
+		table_df=pd.read_csv(output_paths[item])
+		this_y = table_df[plot_column_name].values
+		table_df['Date/Time']='2002'+table_df['Date/Time']
+		table_df['Date/Time']=table_df['Date/Time'].apply(eplus_to_datetime)
+		data_st_date = table_df.iloc[0]['Date/Time']
+		data_ed_date = table_df.iloc[-1]['Date/Time']
+		date_list = table_df['Date/Time']
+		axs.plot(date_list, this_y,
+	            alpha = 0.7,
+	            linestyle = '--',
+	            linewidth = 2,
+	            label = item)
+	datetime_ax_loc = mdates.HourLocator()  
+	datetime_ax_fmt = mdates.DateFormatter('%H:%M')
+	axs.xaxis.set_major_locator(datetime_ax_loc)
+	axs.xaxis.set_major_formatter(datetime_ax_fmt)
+	for tick in axs.xaxis.get_major_ticks():
+		tick.label.set_fontsize(fontsize*0.8) 
+	for tick in axs.yaxis.get_major_ticks():
+		tick.label.set_fontsize(fontsize*0.8) 
+	axs.tick_params('x', labelrotation=45)
+	axs.set_xlabel('Time (%s to %s)'%(data_st_date, data_ed_date),
+	              fontsize = fontsize)
+	axs.set_ylabel(y_axis_title,
+	              fontsize = fontsize)
+	axs.set_title(plot_title)
+	axs.legend(fontsize = fontsize)
+	plt.show()
